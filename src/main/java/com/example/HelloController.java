edc80676ac0325007acba7056bf730d7845c3033
licenfeng <licenfeng@126.com>
2019-05-29 15:44:28 +0800

    Initial commit

---
+++ src/main/java/com/example/HelloController.java
@@ -0,0 +1,15 @@
+package com.example;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ * Created by user on 2019/4/15.
+ *
+ * @Author user
+ */
+@Controller
+public class HelloController {
+    public String hello(){
+        return "";
+    }
+}
